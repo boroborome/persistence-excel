206f85f9dfa776d7b8b8b6d51ca8f2246d3f450e
Yushan Gao <ysgao@thoughtworks.com>
2020-11-09 13:31:08 +0800

    doc: add more doc

--- src/main/java/com/happy3w/persistence/excel/RdConfigInfo.java
+++ src/main/java/com/happy3w/persistence/excel/RdConfigInfo.java
@@ -10,11 +10,33 @@ import org.apache.poi.ss.usermodel.CellStyle;
 import java.util.function.BiConsumer;
 import java.util.function.Function;
 
+/**
+ * 配置处理信息。SheetPage用这些信息让各种配置在Excel上生效
+ * @param <VT> 需要处理数据的数据类型
+ * @param <CT> 对应配置的类型
+ */
 @Getter
 @AllArgsConstructor
 class RdConfigInfo<VT, CT extends IRdConfig> implements ITypeItem<VT> {
+    /**
+     * 需要处理数据的数据类型
+     */
     private Class<VT> type;
+
+    /**
+     * 对应配置的类型
+     */
     private Class<CT> configType;
+
+    /**
+     * 将这个而配置应用到CellStyle上
+     */
     private TernaryConsumer<CellStyle, CT, Function<String, Short>> styleBuilder;
+
+    /**
+     * 标示当前配置是否属于格式配置<br>
+     *     一个Cell上只能有一个格式配置，因此，多个格式配置之间互相冲突。使用的时候只有优先级最高的生效<br>
+     *     生效顺序：Cell上配置、Column配置、Sheet上配置。分别对应某次写入是特别指定的配置，字段上的注解，类上的注解与Page上的配置。
+     */
     private boolean isDataFormat;
 }
