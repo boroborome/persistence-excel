acba7b3744f7908abfe125788526b7f4839a6176
Yushan Gao <ysgao@thoughtworks.com>
2020-11-15 10:34:40 +0800

    chore: String can be read from number cell

--- src/main/java/com/happy3w/persistence/excel/access/StringCellAccessor.java
+++ src/main/java/com/happy3w/persistence/excel/access/StringCellAccessor.java
@@ -2,6 +2,7 @@ package com.happy3w.persistence.excel.access;
 
 import com.happy3w.persistence.core.rowdata.ExtConfigs;
 import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellType;
 
 public class StringCellAccessor implements ICellAccessor<String> {
     @Override
@@ -11,6 +12,9 @@ public class StringCellAccessor implements ICellAccessor<String> {
 
     @Override
     public String read(Cell cell, Class<?> valueType, ExtConfigs extConfigs) {
+        if (cell.getCellTypeEnum() != CellType.STRING) {
+            cell.setCellType(CellType.STRING);
+        }
         return cell.getStringCellValue();
     }
 
